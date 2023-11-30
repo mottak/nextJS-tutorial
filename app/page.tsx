67addcaf29f13e4f1ab8e7e20d53ee97298d32dc
mottak <kissylamottapereira@gmail.com>
2023-11-30 11:15:54 -0300

    refac: retira font lusitana

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import styles from '@/app/ui/home.module.css'
-import { lusitana } from '@/app/ui/fonts'
+// import { lusitana } from '@/app/ui/fonts'
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -16,7 +16,7 @@ export default function Page() {
         <div
           className={styles.shappe}
         />
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
